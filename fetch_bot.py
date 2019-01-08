35de20e8f74e0b2a4430be978aeedb7de265baea
cuongdaovan <cuongdaovan262@gmail.com>
2019-01-08 10:28:51 +0700

    complete order feature

--- fetch_bot.py
+++ fetch_bot.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import time
 from threading import Thread
 import threading
+import random
 
 from skpy import Skype, SkypeChats
 from skpy import SkypeContact, SkypeAuthException
@@ -12,32 +13,38 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 class Bot:
     print('run')
-    fetch_admin = None
-    # fetch_id = "19:6fc079db903d48babebfa404621b1457@thread.skype"  # real
-    fetch_id = "19:b098950b91e14699b97011b951b2b3aa@thread.skype"  # test
+    fetch_id = "19:6fc079db903d48babebfa404621b1457@thread.skype"  # real
+    # fetch_id = "19:b098950b91e14699b97011b951b2b3aa@thread.skype"  # test
+    # fetch_id = "19:87c093e9ad0443ba97b7f2d756ace3a5@thread.skype"
     error_id = "19:87c093e9ad0443ba97b7f2d756ace3a5@thread.skype"  # error
-    fetch_group = None
-    fetch_error = None
     option = 1000
-    username = ''  # lay ra ten user da gui
-    sheet = None  # get data from sheet
-    all_key = ''  # get all key from spreadsheet
-    spams = {}
     scope = [
         'https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive',
     ]
-    creds = None
-    client = None
-    list_order = None
+
+    def __init__(self):
+        self.fetch_admin = None
+        self.fetch_group = None
+        self.fetch_error = None
+        self.sheet = None  # get data from sheet
+        self.creds = None
+        self.client = None
+        self.list_order = None
+        self.connect()
+        self.refresh()
 
     def connect(self):
         try:
+            # self.fetch_admin = Skype(
+            #     'cuongdaovan262@gmail.com', 'developer26297@')
             self.fetch_admin = Skype(
+                'cuongdaovan262@gmail.com',
+                'developer26297@',
                 tokenFile='token.txt',
                 connect=True
             )  # skype account
-            # group chat
+            self.fetch_admin.conn.setTokenFile('token.txt')
             self.fetch_group = self.fetch_admin.chats[self.fetch_id]
             # group error
             self.fetch_error = self.fetch_admin.chats[self.error_id]
@@ -56,6 +63,7 @@ class Bot:
             self.client = gspread.authorize(self.creds)
             s = self.client.open('fetch').sheet1
             self.sheet = s.get_all_records()
+            self.list_dish()
             time.sleep(60)
 
     def all_key(self):
@@ -109,13 +117,12 @@ class Bot:
                 msg += dic['answer']
         self.sendMsg(self.fetch_group, msg=msg, rich=True, typing=True)
 
-    def order(self, msg, user=''):
+    def list_dish(self):
         self.creds = ServiceAccountCredentials.from_json_keyfile_name(
             'fetch_sheet.json', self.scope)
         self.client = gspread.authorize(self.creds)
         sheet_menu = self.client.open('menu')
         worksheet = sheet_menu.get_worksheet(0)
-        sheet_order = self.client.open('order').sheet1
         rice = {}
         for dic in worksheet.get_all_records():
             if dic['key'].strip('\n') == 'rice food':
@@ -133,45 +140,80 @@ class Bot:
         result.update({'rice': rice})
         result.update({'other': other})
         self.list_order = result
-        order_rice = msg.split(' ')
-        print(order_rice)
-        try:
-            if order_rice[0] == 'rice':
-                order = 'cơm hộp: '
-                cost = 0
-                right = True
-                if int(order_rice[1]) < len(result['rice']['vegetables']) and int(order_rice[1]) > 0:
-                    order += result['rice']['vegetables'][int(
-                        order_rice[1])] + " + "
-                else:
-                    right = False
-                if int(order_rice[2]) < len(result['rice']['rice food']) and int(order_rice[2]) > 0:
-                    order += result['rice']['rice food'][int(order_rice[2])]
-                else:
-                    right = False
-                if order_rice[3] in result['rice']['cost']:
-                    cost = int(order_rice[3])
-                else:
-                    right = True
-                if right == True:
+
+    def order(self, msg, user=''):
+        if msg.lower().startswith('-order -rice'):
+            check = True
+            sheet_order = self.client.open('Order cơm trưa').sheet1
+            try:
+                ms = msg[len('-order -rice'):]
+                order_rice = ms.split(',')
+                order_rice = [str(i).lstrip(' ').rstrip(' ') for i in order_rice]
+                v = self.list_order['rice']['vegetables']
+                r = self.list_order['rice']['rice food']
+                for i in order_rice[0].split(' '):
+                    r = [s for s in r if i.lower() in s.lower()]
+                for i in order_rice[1].split(' '):
+                    v = [s for s in v if i.lower() in s.lower()]
+                vegetable = random.choice(v)
+                try:
+                    cost = order_rice[2]
+                    if cost in self.list_order['rice']['cost']:
+                        pass
+                    else:
+                        cost = '30'
+                except IndexError as e:
+                    cost = '30'
+                    print('cost error')
+                rice = random.choice(r)
+                
+                if vegetable == '':
+                    check = False
+                if rice == '':
+                    check = False
+                if check == True:
                     cell = sheet_order.find(user)
-                    sheet_order.update_acell('C'+str(cell.row), order)
-                    sheet_order.update_acell('D'+str(cell.row), '')
-                    sheet_order.update_acell('E'+str(cell.row), cost)
-                    sheet_order.update_acell(
-                        'F'+str(cell.row), 'chưa thanh toán')
-            if order_rice[0] == 'other':
-                if int(order_rice[1]) < len(result['other']):
+                    sheet_order.update_acell('C'+str(cell.row), str(rice))
+                    sheet_order.update_acell('D'+str(cell.row), str(vegetable))
+                    sheet_order.update_acell('E'+str(cell.row), 'Cơm hộp')
+                    sheet_order.update_acell('F'+str(cell.row), '')
+                    sheet_order.update_acell('G'+str(cell.row), cost)
+                    sheet_order.update_acell('H'+str(cell.row), 'Chưa Thanh toán')
+            except ValueError as e:
+                self.sendMsg(self.fetch_error, msg='order error: ' + e, rich=False, typing=True)
+        if msg.lower().startswith('-order -other'):
+            try:
+                result = self.list_order['other'].keys()
+                ms = msg[len('-order -other'):]
+                order = ms.split(' ')
+                for i in order:
+                    result = [s for s in result if i.lower() in s.lower()]
+                other = random.choice(result)
+                if other != '':
+                    sheet_order = self.client.open('Order cơm trưa').sheet1
                     cell = sheet_order.find(user)
                     sheet_order.update_acell('C'+str(cell.row), '')
-                    sheet_order.update_acell(
-                        'D'+str(cell.row), list(result['other'].keys())[int(order_rice[1])])
-                    sheet_order.update_acell(
-                        'E'+str(cell.row), list(result['other'].values())[int(order_rice[1])])
-                    sheet_order.update_acell(
-                        'F'+str(cell.row), 'chưa thanh toán')
-        except Exception:
-            print('error value')
+                    sheet_order.update_acell('D'+str(cell.row), '')
+                    sheet_order.update_acell('E'+str(cell.row), '')
+                    sheet_order.update_acell('F'+str(cell.row), other)
+                    sheet_order.update_acell('G'+str(cell.row), self.list_order['other'][other])
+                    sheet_order.update_acell('H'+str(cell.row), 'Chưa Thanh toán')
+            except Exception as e:
+                self.sendMsg(self.fetch_error, msg='order other error: '+e, rich=False, typing=True)
+    
+    def refresh(self):
+        self.fetch_admin.conn.refreshSkypeToken()
+        self.fetch_admin.conn.writeToken()
+        self.fetch_admin.conn.setTokenFile('token.txt')
+
+    def refreshToken(self):
+        while True:
+            try:
+                self.refresh()
+                print(self.fetch_admin.conn.tokenExpiry)
+            except SkypeAuthException as e:
+                pass
+            time.sleep(100)
 
     def notify(self):
         """
@@ -184,15 +226,14 @@ class Bot:
                 days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
                 if now.strftime("%A").lower() in days:
                     if self.check_time(datetime.now().strftime("%H:%M:%S"), "09:00:01"):
-                        print('true')
                         msg = 'chúc <at id="*">all</at> một ngày làm việc vui vẻ và hiệu quả\n'
-                        msg += "xem các option: @Fetch_admin --help"
+                        msg += "xem các option: -admin --help"
                         self.sendMsg(self.fetch_group, msg=msg,
                                      rich=True, typing=True)
                         time.sleep(1)
                     if self.check_time(datetime.now().strftime("%H:%M:%S"), "10:00:05"):
                         for dic in self.sheet:
-                            if dic['key'].find('order') >= 0:
+                            if dic['key'].find('đặt cơm') >= 0:
                                 self.sendMsg(
                                     self.fetch_group, msg=dic['answer'], rich=True, typing=True)
                         time.sleep(1)
@@ -226,15 +267,13 @@ class Bot:
                         if hasattr(event, 'msg'):  # kiêm tra xem tồn tại msg variable không
                             msg = event.msg
                             content = msg.content
-                            print(type(msg.user.name.__str__()))
                             if msg.chat.id == self.fetch_id:
                                 if content != None:
                                     # kiểm tra xem có mark @Fetch_admin không
-                                    if content.startswith('<at id="8:live:cuongdaovan262">Fetch_admin</at>'):
+                                    if content.startswith('-admin'):
                                         # cut take msg
                                         mess = content[len(
-                                            '<at id="8:live:cuongdaovan262">Fetch_admin</at> '):]
-                                        print(mess)
+                                            '-admin '):]
                                         try:
                                             self.option = int(mess)
                                             print('int')
@@ -250,7 +289,7 @@ class Bot:
                                                         self.update_order()
                                                     self.option = 10000
                                             # kiểm tra xem có key trong list key khong
-                                            if mess.lower().find(dic['key'].lower()) >= 0:
+                                            if mess.lower().find(dic['key'].lower().strip('\n')) >= 0:
                                                 self.fetch_group.sendMsg(
                                                     dic['answer'], rich=True)
                                         if mess.find('--help') >= 0:
@@ -262,37 +301,35 @@ class Bot:
                                                 self.fetch_group, msg=msg, rich=True, typing=True)
                                         if mess.find('update order') >= 0:
                                             self.update_order()
-                                        if mess.find('rice') or mess.find('other'):
-                                            # self.order(
-                                            #     mess, msg.user.name.__str__())
-                                            pass
-                                        if mess.find('món ăn'):
-                                            v = 'vegetables: \n'
-                                            for dish in self.list_order['rice']['vegetables']:
-                                                v += str(self.list_order['rice']['vegetables'].index(
-                                                    dish)) + ".  " + dish + '\n'
-                                            print(v)
-                                            r = 'rice: \n'
-                                            for dish in self.list_order['rice']['rice food']:
-                                                r += str(self.list_order['rice']['rice food'].index(
-                                                    dish)) + ".  " + dish + '\n'
-                                            print(r)
-                                            self.sendMsg(
-                                                self.fetch_group, msg=v+r, rich=False, typing=True)
-
+                                        if mess.find('món ăn') >= 0:
+                                            if self.list_order != None:
+                                                v = 'vegetables: \n'
+                                                for dish in self.list_order['rice']['vegetables']:
+                                                    v += dish + '\n'
+                                                r = 'rice: \n'
+                                                for dish in self.list_order['rice']['rice food']:
+                                                    r += dish + '\n'
+                                                other = 'other: \n'
+                                                for k in self.list_order['other']:
+                                                    other += str(k) + ': ' + str(self.list_order['other'][k]) + '\n'
+                                                self.sendMsg(
+                                                    self.fetch_group, msg=v+r+other, rich=False, typing=True)
+                                    if content.startswith('-order'):
+                                        self.order(content, str(msg.user.name))
                     events = []
                 time.sleep(0.5)
             except Exception as e:
-                self.sendMsg(self.fetch_error, msg=e,
+                self.sendMsg(self.fetch_error, msg='msg error: ' + str(e),
                              rich=False, typing=False)
 
 
 bot = Bot()
-bot.connect()
 t1 = threading.Thread(target=bot.sheet_update)
 t2 = threading.Thread(target=bot.notify)
 t3 = threading.Thread(target=bot.msg)
+t = threading.Thread(target=bot.refreshToken)
 
+t.start()
 t1.start()
 t2.start()
 t3.start()
